12dd66ac02e41065487d1a41fe39d825c74aadee
maharanidevimack-cloud <maharanidevimack@gmail.com>
2026-08-29 20:48:05 +0530

    Update server.js

--- server.js
+++ server.js
@@ -3,6 +3,8 @@
 // ==========================================
 const express = require('express');
 const app = express();
+const cors = require('cors');
+app.use(cors());
 
 app.use(express.json());
 
